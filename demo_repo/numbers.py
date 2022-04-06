5cd2c7c76da2b92e915cb2007b1eb5825b30faab
Eric Hunsberger <eric.hunsberger@appliedbrainresearch.com>
2022-04-06 12:23:23 -0400

    Add some demo_repo code

---
+++ demo_repo/numbers.py
@@ -0,0 +1,25 @@
+class Number:
+    def __init__(self, value):
+        self.value = value
+
+    def add(self, other):
+        return Number(self.value + other.value)
+
+    def __str__(self):
+        return f"N({self.value})"
+
+    def __repr__(self):
+        return f"N(value={self.value})"
+
+
+class Integer(Number):
+    def __init__(self, value):
+        super().__init__(value=int(value))
+
+
+def do_some_thing(number):
+    number = number.add(Number(2))
+    for i in range(number.value):
+        number = number.add(Number(i))
+
+    return number
